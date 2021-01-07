bef2a29c2ade07902b44356b6877463e194fb12f
ORuan <ruanpablomp@gmail.com>
2021-01-06 23:04:10 -0300

    fix error in import

--- GerenciadorTarefas/urls.py
+++ GerenciadorTarefas/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from app.tarefas_views import index
+from app.views.tarefas_views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
